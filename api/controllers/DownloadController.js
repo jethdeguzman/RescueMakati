f3d6cb8ab7de41fd3c29d1a79b4d0e5c04714c20
Jeth <jethdeguzman@gmail.com>
2014-02-07 00:11:37 +0800

    download app

---
+++ api/controllers/DownloadController.js
@@ -0,0 +1,27 @@
+/**
+ * DownloadController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+  index : function(req, res){
+	var path = require('path');
+	var mime = require('mime');
+	var file = __dirname + '/assets/images/screen.png';
+	res.send(file);
+	// var filename = path.basename(file);
+	// var mimetype = mime.lookup(file);
+
+	// res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+	// res.setHeader('Content-type', mimetype);
+
+	// var filestream = fs.createReadStream(file);
+	// filestream.pipe(res);
+
+  }
+  
+
+};
