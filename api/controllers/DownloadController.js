1a90109dff223b03b584cc94033a97ab3f0ed11c
Jeth <jethdeguzman@gmail.com>
2014-02-07 08:18:53 +0800

    download

--- api/controllers/DownloadController.js
+++ api/controllers/DownloadController.js
@@ -8,20 +8,42 @@
 module.exports = {
 
   index : function(req, res){
-	var path = require('path');
-	var mime = require('mime');
-	var appDir = path.dirname(require.main.filename);
-	var file = appDir + '/assets/app/Makati168.apk';
-	// res.send(file);
-	var filename = path.basename(file);
-	var mimetype = mime.lookup(file);
+	// var path = require('path');
+	// var mime = require('mime');
+	// var fs = require('fs');
+	// // var appDir = path.dirname(require.main.filename);
+	// // var file = appDir + '/assets/app/Makati168.apk';
+	// // // res.send(file);
+	// // var filename = path.basename(file);
+	// // var mimetype = mime.lookup(file);
 
-	res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-	res.setHeader('Content-type', mimetype);
+	// // res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+	// // res.setHeader('Content-type', mimetype);
 
-	var filestream = fs.createReadStream(file);
-	filestream.pipe(res);
+	// var filestream = fs.createReadStream(file);
+	// // filestream.pipe(res);
 
+	// var downloader = require('downloader');
+
+	// var downloadDir = '/home/jeth/Desktop/';
+	// // res.send(downloadDir);
+	// downloader.on('done', function(msg) {
+	//     console.log(msg);
+	// });
+
+	// downloader.on('error', function(msg) {
+	//     console.log(msg);
+	// });
+
+	// downloader.download("http://rescuemakati.cloudapp.net/images/gallery/1391514522194.jpg.jpg", filestream);
+ 	var http = require('http');
+ 	var fs = require('fs');
+
+ 	var file = fs.createWriteStream("file.png");
+ 	var request = http.get("http://rescuemakati.cloudapp.net/images/gallery/1391514522194.jpg.jpg", function(response) {
+ 	  response.pipe(file);
+ 	});
+ 	res.send("ok");
   }
   
 
